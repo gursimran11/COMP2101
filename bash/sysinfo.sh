a9f0b7a538732242cdaa02ddcb1d1226efb0d1fe
Gursimran <gursimrank1213@gmail.com>
2023-02-16 11:08:45 -0500

    2nd script

--- bash/sysinfo.sh
+++ bash/sysinfo.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 echo "My second script"
-#dat1 is a variable which will give the hostname
+#sim1 is a variable for hostname
 sim1=$(hostname)
-#dat2 is a variable which will give the fully qualified domain name(FQDN)
+#dat2 is a variable for fully qualified domain name(FQDN)
 sim2=$(hostname)
-#dat 3 will give the whole information regarding the system and grep will trim it operating system information
 sim3=$(hostnamectl | grep Operating)
-#dat4  is a variable which will provide the IP address of the host name
+#sim4  is a variable which will provide the IP address of the host name and not the 127 one
 sim4=$(hostname -I)
-#dat5 is a variable which will give us the system indormation in which awk is used to give only the fourth line and then tail is used because we only want the last line
+#sim5 is a variable which will give us the system information
 sim5=$(df -h / | tail -1 | awk '{print $4}')
 cat <<EOF
 Report for:$sim1
